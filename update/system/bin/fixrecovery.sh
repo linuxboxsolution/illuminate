4dff5b61ec9b0aaedaf376aace63a2f93ddb0e0e
root <root@linuxboxsolution.com>
2011-07-27 17:02:53 -0400

    Intial Setting up of repo. Starting with Android Illuminate 3.0

---
+++ update/system/bin/fixrecovery.sh
@@ -0,0 +1,2 @@
+/system/bin/flash_image recovery /system/etc/recovery/recovery.img
+	
---
+++ update/system/etc/script.sh
@@ -0,0 +1,2 @@
+echo 2048 > /sys/devices/virtual/bdi/179:16/read_ahead_kb
+echo 2048 > /sys/devices/virtual/bdi/179:24/read_ahead_kb
